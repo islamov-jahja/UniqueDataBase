0ee60adcaaf1b998cb1d42ea0730d17d0b968cce
yahya <yahyaislamov15@mail.ru>
2019-05-14 10:49:06 +0300

    добавил клиента и класс для работы с базой данных

---
+++ UniqueDataBase/DataBase/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DataBase
+{
+    public class Class1
+    {
+        private String _dataBaseHost;
+        Class1(String dataBaseHost)
+        {
+            _dataBaseHost = dataBaseHost;
+        }
+    }
+}
---
+++ UniqueDataBase/client1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DataBase;
+
+namespace client1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
