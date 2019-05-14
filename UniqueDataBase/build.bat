392dcc7036fa6ebbc4f10a4c0604e153bf0c40e5
yahya <yahyaislamov15@mail.ru>
2019-05-14 10:53:12 +0300

    изменил батник

--- UniqueDataBase/build.bat
+++ UniqueDataBase/build.bat
@@ -4,7 +4,9 @@ set version=%1
 if defined version (
     cd KeyValueDatabase
     dotnet publish
-	cd ..
+	cd ../client1
+    dotnet publish
+    cd ..
 
 	if not exist build_versions (
 		mkdir build_versions
@@ -24,6 +26,9 @@ if defined version (
         echo KeyValueDatabase:5000> config.txt
 		echo KeyValueDatabase:5001>> config.txt
 
+        echo cd client1 > run.bat
+        echo start "client1" dotnet client1.dll >> run.bat
+		echo cd .. >> run.bat
 		echo call run_servers_with_dB.bat >> run.bat
 
 		echo taskkill /IM dotnet.exe /F > stop.bat
